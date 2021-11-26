733f794c795eac3781b9b7ff73511d1e474751c6
matheus.monteiro <matheus.monteiro@PC07751>
2021-11-26 15:51:22 -0300

    Add TextNumbers

---
+++ ValidateBRDocuments/TextNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using Caelum.Stella.CSharp.Inwords;
+using System;
+using System.Diagnostics;
+
+namespace TextNumbers
+{
+    // this program use a library for transform int, float and double numbers in write numbers. 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor = 75;
+            string extenso =  new Numero(valor).Extenso();
+            Debug.WriteLine(extenso);
+
+            valor = 1532987;
+            extenso = new Numero(valor).Extenso();
+            Debug.WriteLine(extenso);
+
+            string extensoBRL = new MoedaBRL(valor).Extenso();
+            Debug.WriteLine(extensoBRL);
+
+            valor = 1532987.89;
+            extensoBRL = new MoedaBRL(valor).Extenso();
+            Debug.WriteLine(extensoBRL);
+
+        }
+    }
+}
