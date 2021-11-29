440fb238ad4788e3e571ac11a6618419c488b895
matheus.monteiro <matheus.monteiro@PC07751>
2021-11-29 16:51:25 -0300

    all concept  example

---
+++ WorkContract/WorkContract/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkContract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
